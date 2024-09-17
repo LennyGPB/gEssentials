2138d3f2b0e22a70a628af3bfe4d12b07662acb0
KuroOzaki <thander294@gmail.com>
2024-09-17 21:11:02 +0200

    Optimise tailwind CSS

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,9 +5,9 @@ export default function NavBar() {
         <nav className="flex justify-between items-center py-1 px-40 bg-navbar text-white">
             <a href="/" className="w-28 h-28"><img src="../images/gessentials.png" alt="gessentials logo"/></a>
             <ul className="flex space-x-12">
-                <li><a href="/formations"  className="text-white hover:text-gray-400" >Nos Formations</a></li>
-                <li><a href="/avis"  className="text-white hover:text-gray-400" >Avis</a></li>
-                <li><a href="/discord"  className="text-white hover:text-gray-400" >Notre discord</a></li>
+                <li><a href="/formations"  className="hover:text-gray-400" >Nos Formations</a></li>
+                <li><a href="/avis"  className="hover:text-gray-400" >Avis</a></li>
+                <li><a href="/discord"  className="hover:text-gray-400" >Notre discord</a></li>
             </ul>
         </nav>
     </>;
