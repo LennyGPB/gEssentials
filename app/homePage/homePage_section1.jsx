a62eae31b1aa2a2892a9083c9f482bd46c49900f
Lenny Gomes <lennygomes.hp@gmail.com>
2024-09-18 14:12:45 +0200

    NavBar + Section 1 WIP

--- app/homePage/homePage_section1.jsx
+++ app/homePage/homePage_section1.jsx
@@ -3,20 +3,28 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Particles from "@/components/magicui/particles";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function HomePage_section1() {
-  const [color, setColor] = useState("#00000");
+  const [color, setColor] = useState("#1d3269");
 
   return (
     <>
       <section>
-        <div className="flex justify-center items-center text-white flex-col">
+        <div className="flex justify-center items-center text-white flex-col pt-10">
           <h1 className="text-center  tracking-widest text-8xl pt-32">
             gESSENTIALS
           </h1>
 
           <h2 className="tracking-widest text-3xl pt-2">
-            Apprenez à nos cotés
+            <RoughNotation
+              type="highlight"
+              show={true}
+              color="#2B4999"
+              animationDuration={2500}
+            >
+              Apprenez à nos cotés
+            </RoughNotation>
           </h2>
 
           <a
@@ -25,7 +33,30 @@ export default function HomePage_section1() {
           >
             CONSULTER NOS FORMATIONS
           </a>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-16 mt-16 animate-bounce"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+            />
+          </svg>
         </div>
+
+        <Particles
+          className="absolute inset-0 z-[-1]"
+          quantity={300}
+          ease={80}
+          color={color}
+          refresh
+        />
       </section>
     </>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -10,12 +10,12 @@ export default function Home() {
     <>
       <NavBar />
       <HomePage_section1 />
-      <Card
+      {/* <Card
         title="Création d'un playermodel avec un script !"
         theme="Modélisation 3D"
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt iusto cum corporis tenetur, voluptatum, provident qui ab nisi fugiat ullam necessitatibus ipsa? Deleniti, doloribus. Minus reprehenderit aliquam enim magnam distinctio?
 Unde perferendis rem deleniti. Suscipit, modi. Dolore vitae voluptas minima corporis asperiores? Debitis perferendis excepturi maiores iste vitae. Tempore esse ipsam vel voluptatum ratione excepturi omnis nam perspiciatis et quaerat."
-      />
+      /> */}
     </>
   );
 }
